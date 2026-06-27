3286c7fc2c1e69f73407d938d6239ebf7476b7d8
vikasl <vikasl@hotmail.com>
2026-06-27 15:58:59 -0400

    Update lesson-02-numbers-and-math.py for Saumyaloomba

--- students/saumyaloomba/lesson-02-numbers-and-math.py
+++ students/saumyaloomba/lesson-02-numbers-and-math.py
@@ -1,6 +1,24 @@
-# Let's work with random numbers
+
+# Lottery 
 import random
 
-roll = random.randint(1,6)
+roll = random.randint(1,5)
 
 print("You rolled:", roll)
+
+yournumber= int(input ("what is your number"))
+if roll== yournumber :
+    print ("you won") 
+else :
+    print ("you lost")
+
+
+
+
+
+
+
+
+
+
+
