86aeaaa60cfcf8711f554e2d5313672ed2dd3f57
vikasl <vikasl@hotmail.com>
2026-06-27 20:46:02 -0400

    Add lesson-03-using-variables.py for 61443880

---
+++ students/61443880/lesson-03-using-variables.py
@@ -0,0 +1,10 @@
+# Variables store information
+name = "Saanvi the great"
+age = 12
+favorite_color= "black"
+favorite_sport= "swimming"
+
+print("My name is", name)
+print("I have a twin but i'm obviously the better twin")
+print("My favorite color is", favorite_color)
+print("My favorite sport is", favorite_sport)
