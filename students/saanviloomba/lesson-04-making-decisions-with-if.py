2997a8cdc48802c3c1af735e7ddbbb078859a46b
vikasl <vikasl@hotmail.com>
2026-07-18 15:54:52 -0400

    Update lesson-04-making-decisions-with-if.py for Saanviloomba

--- students/saanviloomba/lesson-04-making-decisions-with-if.py
+++ students/saanviloomba/lesson-04-making-decisions-with-if.py
@@ -1,13 +1,6 @@
-# Making decisions
-temperature = 25
-
-if temperature > 20:
-    print("It's warm outside!")
+import random
+flip = random.choice(["Heads", "Tails"])
+if flip == "Heads":
+       print("🟡 HEADS!")
 else:
-    print("Better wear a jacket!")
-
-age = 12
-if age >= 13:
-    print("You are a teenager!")
-else:
-    print("Not a teenager yet!")
+       print("⚪ TAILS!")
