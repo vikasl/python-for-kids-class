797b565f205d8871cb42f5dd17e695f61803c465
vikasl <vikasl@hotmail.com>
2026-07-18 15:54:47 -0400

    Add lesson-04-making-decisions-with-if.py for Saanviloomba

---
+++ students/saanviloomba/lesson-04-making-decisions-with-if.py
@@ -0,0 +1,13 @@
+# Making decisions
+temperature = 25
+
+if temperature > 20:
+    print("It's warm outside!")
+else:
+    print("Better wear a jacket!")
+
+age = 12
+if age >= 13:
+    print("You are a teenager!")
+else:
+    print("Not a teenager yet!")
